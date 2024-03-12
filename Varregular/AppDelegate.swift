539494be1afae0eaaa655551491a612419a1c77c
balandzin <balandzin2602@gmail.com>
2024-03-12 22:20:47 +0300

    initial commit

---
+++ Varregular/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Varregular
+//
+//  Created by Антон Баландин on 12.03.24.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let navigationController = UINavigationController()
+        navigationController.viewControllers = [ViewController()]
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+

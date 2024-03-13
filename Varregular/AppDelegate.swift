bfab76126d09ace6458eb086929bcb7802fa90d5
balandzin <balandzin2602@gmail.com>
2024-03-13 19:03:26 +0300

    Start GUI

--- Varregular/AppDelegate.swift
+++ Varregular/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         let navigationController = UINavigationController()
-        navigationController.viewControllers = [ViewController()]
+        navigationController.viewControllers = [HomeViewController()]
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = navigationController
---
+++ Varregular/HomeViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Varregular
+//
+//  Created by Антон Баландин on 12.03.24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    // MARK: - GUI Variables
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Lifecycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    // MARK: - Private Methods
+     
+}
+

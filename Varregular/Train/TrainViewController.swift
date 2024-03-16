08742b404a912bd31c56877c36bdd1fc0c989178
balandzin <balandzin2602@gmail.com>
2024-03-16 12:46:35 +0300

    Реализовал логику поднятия и опускания клавиатуры и скрытия по жесту(тап)

--- Varregular/Train/TrainViewController.swift
+++ Varregular/Train/TrainViewController.swift
@@ -6,15 +6,178 @@
 //
 
 import UIKit
+import SnapKit
 
 final class TrainViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    private lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        view.showsVerticalScrollIndicator = false
+        
+        return view
+    }()
+    
+    private lazy var contentView: UIView = UIView()
+    
+    private lazy var infinitiveLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 28)
+        label.textColor = .black
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private lazy var pastSimpleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .gray
+        label.text = "Past Simple"
+        return label
+    }()
+    
+    private lazy var participleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .gray
+        label.text = "Past Participle"
+        return label
+    }()
+    
+    private lazy var pastSimpleTextField: UITextField = {
+        let field = UITextField()
+        field.borderStyle = .roundedRect
+        field.delegate = self
+        return field
+    }()
+    
+    private lazy var pastParticipleTextField: UITextField = {
+        let field = UITextField()
+        field.borderStyle = .roundedRect
+        field.delegate = self
+        return field
+    }()
+    
+    private lazy var checkButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Check".localized, for: .normal)
+        button.setTitleColor(UIColor.black, for: .normal)
+        button.backgroundColor = .systemGray5
+        button.layer.cornerRadius = 10
+        return button
+    }()
+    
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "Select verbs".localized
+        
+        setupUI()
+        
+        registerForKeyboardNotifications()
+        unregisterForKeyboardNotifications()
+        hideKeyboardWhenTappedAround()
+    }
+    
+    // MARK: - Private Methods
+    private func setupUI() {
+        view.backgroundColor = .white
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubviews([
+            infinitiveLabel,
+            pastSimpleLabel,
+            pastSimpleTextField,
+            participleLabel,
+            pastParticipleTextField,
+            checkButton
+        ])
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        scrollView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.size.edges.equalToSuperview()
+        }
+        
+        infinitiveLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().inset(200)
+            make.centerX.equalToSuperview()
+        }
+        
+        pastSimpleLabel.snp.makeConstraints { make in
+            make.top.equalTo(infinitiveLabel.snp.bottom).offset(60)
+            make.leading.trailing.equalToSuperview().inset(30)
+        }
+        
+        pastSimpleTextField.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.top.equalTo(pastSimpleLabel.snp.bottom).offset(10)
+        }
+        
+        participleLabel.snp.makeConstraints { make in
+            make.top.equalTo(pastSimpleTextField.snp.bottom).offset(20)
+            make.leading.trailing.equalToSuperview().inset(30)
+        }
+        
+        pastParticipleTextField.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.top.equalTo(participleLabel.snp.bottom).offset(10)
+        }
+        
+        checkButton.snp.makeConstraints { make in
+            make.leading.trailing.equalToSuperview().inset(30)
+            make.top.equalTo(pastParticipleTextField.snp.bottom).offset(100)
+        }
+    }
+}
 
-        view.backgroundColor = .yellow
+// MARK: - UITextFieldDelegate
+extension TrainViewController: UITextFieldDelegate {
+    // TODO
+}
+
+// MARK: - Keyboard events
+private extension TrainViewController {
+    func registerForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillShow(_:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+    }
+    
+    func unregisterForKeyboardNotifications() {
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillHide),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
+    }
+    
+    @objc func keyboardWillShow(_ notification: Notification) {
+        guard let frame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        scrollView.contentInset.bottom = frame.height + 50
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        scrollView.contentInset.bottom = .zero - 50
+    }
+    
+    func hideKeyboardWhenTappedAround() {
+        let recognizer = UITapGestureRecognizer(target: self, action: #selector(hideKeyboard))
+        scrollView.addGestureRecognizer(recognizer)    }
+    
+    @objc func hideKeyboard() {
+        scrollView.endEditing(true)
     }
 }
